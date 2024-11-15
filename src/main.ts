75228a0fbbd4e84eeca7fd9a2f4e52f49add5f43
Piotr Squares Mościcki <piotr.squares.moscicki@gmail.com>
2024-11-15 18:23:13 +0000

    Fix npm run test command

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-function displayHelloWorld(): string {
+export function displayHelloWorld(): string {
     return "Hello world";
 }
 
---
+++ src/test/main.test.ts
@@ -0,0 +1,7 @@
+import { displayHelloWorld } from '../main';
+
+describe('displays "Hello world', () => {
+    test('displays "Hello world', () => {
+        expect(displayHelloWorld()).toBe('Hello world');
+    });
+});
